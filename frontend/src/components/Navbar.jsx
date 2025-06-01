e0584101b28dd091834cfbdc78200998960088bc
sanic250 <115185839+sanic250@users.noreply.github.com>
2025-06-01 23:04:37 +0200

    first commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import styles from '../styles/navbar.module.css'
+import React from "react";
+import styles from "../styles/navbar.module.css";
+import { Search, ShoppingBasket, Heart, KeyRound, UserRoundPen} from "lucide-react";
 const Navbar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src="/logoaudio3.png" alt="logo" className={styles.logo} />
+        <div className={styles.search}>
+          <form className={styles.form}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Search products..."
+            />
+            <button className={styles.btnSearch} type="submit">
+              <Search className={styles.iconSearch} />
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className={styles.profile}>
+      <div className={styles.cart}>
+        <ShoppingBasket className={`${styles.cartIcon} ${styles.icons}`} />
+      </div>
+      <div className={styles.favorites}>
+        <Heart className={`${styles.hearth} ${styles.icons}`} />
+      </div>
+      <div className={styles.loginProfile}>
+        <button className={`${styles.login} ${styles.buttons}`}><KeyRound /> Login</button>
+        <button className={`${styles.register} ${styles.buttons}`}><UserRoundPen /> Register</button>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
